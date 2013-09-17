b550362bb87e34e05b3a96e0be50ca5bc6dd6084
abc549825 <abc549825@163.com>
2013-09-17 12:26:02 +0800

    fix xiechao06/lite-mms#143 refactor

--- src/com/jinheyu/lite_mms/TeamLeaderMenuItemWrapper.java
+++ src/com/jinheyu/lite_mms/TeamLeaderMenuItemWrapper.java
@@ -19,65 +19,94 @@ public class TeamLeaderMenuItemWrapper {
     }
 
     public void carryForward(final int workCommandId) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mActivity);
-        alertDialogBuilder.setNegativeButton(android.R.string.cancel, null).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
-        builder.msg(String.format("工单%d请求结转中", workCommandId)).run(new XProgressableRunnable.XRunnable() {
-            @Override
-            public void run() throws Exception {
-                MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_CARRY_FORWARD, null);
-            }
-        }).okMsg(mActivity.getString(R.string.carryForward_success, workCommandId)).create().start();
-            }
-        }).show();
-
+        newBuilder(mActivity.getString(R.string.confirm_carry_forward, workCommandId),
+                String.format("工单%d请求结转中", workCommandId),
+                new XProgressableRunnable.XRunnable() {
+                    @Override
+                    public void run() throws Exception {
+                        MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_CARRY_FORWARD, null);
+                    }
+                },
+                mActivity.getString(R.string.carryForward_success, workCommandId)
+        ).show();
     }
 
     public void carryForward(final int[] workCommandIds) {
-        XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
-        String workCommands_str = Arrays.toString(workCommandIds);
-        builder.msg(String.format("工单%s批量结转中", workCommands_str)).run(new XProgressableRunnable.XRunnable() {
-            @Override
-            public void run() throws Exception {
-                for (int workCommandId : workCommandIds) {
-                    MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_CARRY_FORWARD, null);
-                }
-            }
-        }).okMsg(mActivity.getString(R.string.carryForward_success, workCommands_str)).create().start();
+        final String workCommands_str = Arrays.toString(workCommandIds);
+
+        newBuilder(mActivity.getString(R.string.confirm_carry_forward, workCommands_str),
+                String.format("工单%s批量结转中", workCommands_str),
+                new XProgressableRunnable.XRunnable() {
+                    @Override
+                    public void run() throws Exception {
+                        for (int workCommandId : workCommandIds) {
+                            MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_CARRY_FORWARD, null);
+                        }
+                    }
+                },
+                mActivity.getString(R.string.carryForward_success, workCommands_str)
+        ).show();
+
     }
 
     public void carryForwardQuickly(final int workCommandId) {
-        XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
-        builder.msg(String.format("工单%d快速结转中", workCommandId)).run(new XProgressableRunnable.XRunnable() {
-            @Override
-            public void run() throws Exception {
-                MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_QUICK_CARRY_FORWARD, null);
-            }
-        }).okMsg(mActivity.getString(R.string.quick_carryForward_success, workCommandId)).create().start();
+        newBuilder(mActivity.getString(R.string.confirm_carry_forward_quickly, workCommandId),
+                String.format("工单%d快速结转中", workCommandId),
+                new XProgressableRunnable.XRunnable() {
+                    @Override
+                    public void run() throws Exception {
+                        MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_QUICK_CARRY_FORWARD, null);
+                    }
+                },
+                mActivity.getString(R.string.quick_carryForward_success, workCommandId)
+        ).show();
     }
 
     public void carryForwardQuickly(final int[] workCommandIds) {
-        String workCommandIds_str = Arrays.toString(workCommandIds);
-        XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
-        builder.msg(String.format("工单%s批量快速结转中", workCommandIds_str)).run(new XProgressableRunnable.XRunnable() {
-            @Override
-            public void run() throws Exception {
-                for (int workCommandId : workCommandIds) {
-                    MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_QUICK_CARRY_FORWARD, null);
-                }
-            }
-        }).okMsg(mActivity.getString(R.string.quick_carryForward_success, workCommandIds_str)).create().start();
+        final String workCommandIds_str = Arrays.toString(workCommandIds);
+
+        newBuilder(mActivity.getString(R.string.confirm_carry_forward_quickly, workCommandIds_str),
+                String.format("工单%s批量快速结转中", workCommandIds_str),
+                new XProgressableRunnable.XRunnable() {
+                    @Override
+                    public void run() throws Exception {
+                        for (int workCommandId : workCommandIds) {
+                            MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_QUICK_CARRY_FORWARD, null);
+                        }
+                    }
+                },
+                mActivity.getString(R.string.quick_carryForward_success, workCommandIds_str)
+        ).show();
     }
 
-    public void endWorkCommand(final int workCommand) {
-        XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
-        builder.msg(String.format("工单%d请求结束中", workCommand)).run(new XProgressableRunnable.XRunnable() {
-            @Override
-            public void run() throws Exception {
-                MyApp.getWebServieHandler().updateWorkCommand(workCommand, Constants.ACT_END, null);
-            }
-        }).okMsg(mActivity.getString(R.string.end_success, workCommand)).create().start();
+    public void endWorkCommand(final int workCommandId) {
+        newBuilder(mActivity.getString(R.string.confirm_end, workCommandId),
+                String.format("工单%d请求结束中", workCommandId),
+                new XProgressableRunnable.XRunnable() {
+                    @Override
+                    public void run() throws Exception {
+                        MyApp.getWebServieHandler().updateWorkCommand(workCommandId, Constants.ACT_END, null);
+                    }
+                },
+                mActivity.getString(R.string.end_success, workCommandId)
+        ).show();
+    }
+
+    private AlertDialog.Builder newBuilder(final String titleString, final String startString, final XProgressableRunnable.XRunnable runnable, final String okString) {
+        return new AlertDialog.Builder(mActivity)
+                .setTitle(titleString)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        XProgressableRunnable.Builder builder = new XProgressableRunnable.Builder(mActivity);
+                        builder.msg(startString).run(runnable).after(new Runnable() {
+                            @Override
+                            public void run() {
+                                mActivity.onNavigateUp();
+                            }
+                        }).okMsg(okString).create().start();
+                    }
+                });
     }
 }
